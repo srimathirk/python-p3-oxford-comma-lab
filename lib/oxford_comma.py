cb627dddae67984a6f5d2587c8341674df5f3b96
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-07-13 20:21:13 -0400

    list to string conversion lab done

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,11 @@
 def oxford_comma(items):
-    return None
+    if len(items) > 3:
+        str_items = ", ".join(items[:-1]) + ", and "+ items[-1]
+    elif len(items) == 3:
+        str_items = f"{items[0]}, {items[1]}, and {items[2]}"  
+    elif len(items) == 2:
+        str_items = " and ".join(items)    
+    else:
+        str_items = " ".join(items)
+    print(str_items)
+    return str_items
